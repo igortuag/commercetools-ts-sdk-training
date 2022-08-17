291592811aa381b75d7f5fdaad518b67cfc83186
Tuag <tuag@imaginationmedia.com>
2022-08-17 12:24:03 -0300

    add custom type

--- Task07a_custom_types.ts
+++ Task07a_custom_types.ts
@@ -3,32 +3,29 @@ import { apiRoot } from "./handson/client";
 import { log } from "./utils/logger";
 
 const typeDraft: TypeDraft = {
-    key: "tt-allowed-to-place-orders",
-    name: {
-        "de": "tt allowed to place orders",
-        "en": "tt allowed to place orders"
+  key: "it-allowed-to-place-orders",
+  name: {
+    de: "it allowed to place orders",
+    en: "it allowed to place orders",
+  },
+  description: {
+    de: "allow a customer to place orders",
+    en: "allow a customer to place orders",
+  },
+  resourceTypeIds: ["customer"],
+  fieldDefinitions: [
+    {
+      type: {
+        name: "Boolean",
+      },
+      name: "allowed-to-place-orders",
+      label: {
+        de: "Allowed to place orders",
+        en: "Allowed to place orders",
+      },
+      required: false,
     },
-    description: {
-        "de": "allow a customer to place orders",
-        "en": "allow a customer to place orders"
-    },
-    resourceTypeIds: ["customer"],
-    fieldDefinitions: [{
-        type: {
-            name: "Boolean"
-        },
-        name: "allowed-to-place-orders",
-        label: {
-            "de": "Allowed to place orders",
-            "en": "Allowed to place orders"
-        },
-        required: false,
-    }]
-}
+  ],
+};
 
-apiRoot
-    .types()
-    .post({ body: typeDraft })
-    .execute()
-    .then(log)
-    .catch(log);
+apiRoot.types().post({ body: typeDraft }).execute().then(log).catch(log);
